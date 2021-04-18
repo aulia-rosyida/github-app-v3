a3a5f963061ab004ef0153233bf33a9ff96fa559
aulia-rosyida <rosyidaaulia24@gmail.com>
2021-04-18 22:39:45 +0700

    map cursor to object

--- GithubUser/app/src/main/java/com/dicoding/auliarosyida/githubuser/helper/MappingHelper.kt
+++ GithubUser/app/src/main/java/com/dicoding/auliarosyida/githubuser/helper/MappingHelper.kt
@@ -1,6 +1,7 @@
 package com.dicoding.auliarosyida.githubuser.helper
 
 import android.database.Cursor
+import android.provider.ContactsContract
 import com.dicoding.auliarosyida.githubuser.db.UserDbContract
 import com.dicoding.auliarosyida.githubuser.entity.User
 import java.lang.Boolean.getBoolean
@@ -25,5 +26,24 @@ object MappingHelper {
             }
         }
         return usersList
+    }
+
+    fun mapCursorToObject(notesCursor: Cursor?): User {
+        var anUser = User()
+        notesCursor?.apply {
+            moveToFirst()
+            val id = getInt(getColumnIndexOrThrow(UserDbContract.UserDbColumns._ID))
+            val uname = getString(getColumnIndexOrThrow(UserDbContract.UserDbColumns.COL_USERNAME))
+            val name = getString(getColumnIndexOrThrow(UserDbContract.UserDbColumns.COL_NAME))
+            val location = getString(getColumnIndexOrThrow(UserDbContract.UserDbColumns.COL_LOCATION))
+            val repo = getString(getColumnIndexOrThrow(UserDbContract.UserDbColumns.COL_REPOSITORY))
+            val company = getString(getColumnIndexOrThrow(UserDbContract.UserDbColumns.COL_COMPANY))
+            val follower = getString(getColumnIndexOrThrow(UserDbContract.UserDbColumns.COL_FOLLOWER))
+            val following = getString(getColumnIndexOrThrow(UserDbContract.UserDbColumns.COL_FOLLOWING))
+            val photo = getString(getColumnIndexOrThrow(UserDbContract.UserDbColumns.COL_PHOTO))
+            val isFavorited = getBoolean(UserDbContract.UserDbColumns.COL_IS_FAVORITED)
+            anUser = User(id, uname, name, location, repo, company, follower, following, photo, isFavorited)
+        }
+        return anUser
     }
 }
