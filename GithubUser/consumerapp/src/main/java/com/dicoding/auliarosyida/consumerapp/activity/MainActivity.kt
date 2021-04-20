67cde785edf23f8c5547cb247ece847b85f52c00
aulia-rosyida <rosyidaaulia24@gmail.com>
2021-04-20 12:49:48 +0700

    change page khusus fav page consumer app

--- GithubUser/consumerapp/src/main/java/com/dicoding/auliarosyida/consumerapp/activity/MainActivity.kt
+++ GithubUser/consumerapp/src/main/java/com/dicoding/auliarosyida/consumerapp/activity/MainActivity.kt
@@ -1,199 +1,118 @@
 package com.dicoding.auliarosyida.consumerapp.activity
 
-import android.content.Intent
+import android.database.ContentObserver
 import android.os.Bundle
-import android.view.Menu
-import android.view.MenuItem
+import android.os.Handler
+import android.os.HandlerThread
 import android.view.View
-import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
 import androidx.recyclerview.widget.LinearLayoutManager
-import com.dicoding.auliarosyida.consumerapp.BuildConfig
-import com.dicoding.auliarosyida.consumerapp.R
-import com.dicoding.auliarosyida.consumerapp.adapter.UserAdapter
+import com.dicoding.auliarosyida.consumerapp.adapter.FavoriteAdapter
 import com.dicoding.auliarosyida.consumerapp.databinding.ActivityMainBinding
+import com.dicoding.auliarosyida.consumerapp.db.UserDbContract.UserDbColumns.Companion.CONTENT_URI
 import com.dicoding.auliarosyida.consumerapp.entity.User
-import com.google.gson.Gson
-import com.loopj.android.http.AsyncHttpClient
-import com.loopj.android.http.AsyncHttpResponseHandler
-import cz.msebera.android.httpclient.Header
-import org.json.JSONObject
-import java.util.*
+import com.dicoding.auliarosyida.consumerapp.helper.MappingHelper
+import com.google.android.material.snackbar.Snackbar
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.GlobalScope
+import kotlinx.coroutines.async
+import kotlinx.coroutines.launch
 
 class MainActivity : AppCompatActivity() {
 
-    private var title: String = "Consumer User List"
-    private var tempSearch = "aulia-"
-
     private lateinit var binding: ActivityMainBinding
-    private var users = mutableListOf<User>()
+    lateinit var adapterFavPage: FavoriteAdapter
+    private var dummyFavorite = User(0,"You do not have favorited user","", "", "","","","","")
 
-    private var listUserAdapter = UserAdapter(users)
-    var dummyUser = User(0,"Please try with another username","Sorry, this username could not been find", "", "","","","","")
+    companion object {
+        private const val EXTRA_STATE = "EXTRA_STATE"
+    }
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         binding = ActivityMainBinding.inflate(layoutInflater)
         setContentView(binding.root)
 
-        setActionBarTitle(title)
-
-        binding.rvList.setHasFixedSize(true)
-        binding.rvList.adapter = listUserAdapter
+        supportActionBar?.title = "Favorite"
         binding.rvList.layoutManager = LinearLayoutManager(this)
-        binding.progressBar.visibility = View.VISIBLE
+        binding.rvList.setHasFixedSize(true)
+        adapterFavPage = FavoriteAdapter(this)
+        binding.rvList.adapter = adapterFavPage
 
-        getUsersApi()
+        val handlerThread = HandlerThread("DataObserver")
+        handlerThread.start()
+        val handler = Handler(handlerThread.looper)
 
-        listUserAdapter.setOnItemClickCallback(object : UserAdapter.OnItemClickCallback{
-            override fun onItemClicked(data: User) {
-                getDetailUserApi(data, data.id)
+        val myObserver = object : ContentObserver(handler) {
+            override fun onChange(self: Boolean) {
+                loadFavoritesAsync()
             }
-        })
+        }
 
-//        binding.searchButton.setOnClickListener {
-//            tempSearch = binding.textInputSearch.text.toString()
-//            getUsersApi()
-//            Toast.makeText(this@MainActivity, "You search $tempSearch.", Toast.LENGTH_SHORT).show()
-//        }
+        contentResolver.registerContentObserver(CONTENT_URI, true, myObserver)
+
+        if (savedInstanceState == null) {
+            loadFavoritesAsync()
+        } else {
+            savedInstanceState.getParcelableArrayList<User>(EXTRA_STATE)?.also { adapterFavPage.listFavorites = it }
+        }
+//        binding.rvFavorites.adapter?.notifyDataSetChanged()
+
+//        supportActionBar?.setDisplayHomeAsUpEnabled(true)
+    }
+
+//    override fun onResume() {
 //
-//        binding.textInputSearch.setOnEditorActionListener { _, actionId, _ ->
-//            if (actionId == EditorInfo.IME_ACTION_SEARCH) {
-//                tempSearch = binding.textInputSearch.text.toString()
-//                getUsersApi()
-//                Toast.makeText(this@MainActivity, "You search $tempSearch.", Toast.LENGTH_SHORT).show()
+//        // proses ambil data
+//        loadFavoritesAsync()
+//        super.onResume()
+//    }
+
+    // function to the button on press
+//    override fun onOptionsItemSelected(menuItem: MenuItem): Boolean {
+//        when (menuItem.itemId) {
+//            android.R.id.home -> {
+//                finish()
+//                return true
 //            }
-//            true
 //        }
+//        return super.onOptionsItemSelected(menuItem)
+//    }
 
-    }
-
-    private fun setActionBarTitle(title: String) {
-        supportActionBar?.title = title
-    }
-
-   override fun onCreateOptionsMenu(menu: Menu): Boolean {
-       val inflater = menuInflater
-       inflater.inflate(R.menu.option_menu, menu)
-       return super.onCreateOptionsMenu(menu)
-   }
-
-    override fun onOptionsItemSelected(item: MenuItem): Boolean {
-        when(item.itemId){
-            R.id.action_change_settings -> {
-                val mIntent = Intent(this@MainActivity, SettingsActivity::class.java)
-                startActivity(mIntent)
-                return true
-            }
-            R.id.favPage -> {
-                val favIntent = Intent(this@MainActivity, FavoritePageActivity::class.java)
-                startActivity(favIntent)
-                return true
-            }
-        }
-        return super.onOptionsItemSelected(item)
-    }
-
-    private fun getUsersApi() {
+    private fun loadFavoritesAsync() {
         binding.progressBar.visibility = View.VISIBLE
-        val client = AsyncHttpClient()
-       client.addHeader("Authorization", "token ${BuildConfig.GITHUB_TOKEN}")
-       client.addHeader("User-Agent", "request")
-        val url = "https://api.github.com/search/users?q=$tempSearch"
 
-        client.get(url, object : AsyncHttpResponseHandler() {
-            override fun onSuccess(statusCode: Int, headers: Array<Header>, responseBody: ByteArray) {
-                // Jika koneksi berhasil
-                binding.progressBar.visibility = View.INVISIBLE
-                // Parsing JSON
-                val result = String(responseBody)
-                getListUsers(result)
+        GlobalScope.launch(Dispatchers.Main) {
 
-            }
-            override fun onFailure(statusCode: Int, headers: Array<Header>, responseBody: ByteArray, error: Throwable) {
-                // Jika koneksi gagal
-                binding.progressBar.visibility = View.INVISIBLE
-                val errorMessage = when (statusCode) {
-                    401 -> "$statusCode : Bad Request"
-                    403 -> "$statusCode : Forbidden"
-                    404 -> "$statusCode : Not Found"
-                    else -> "$statusCode : ${error.message}"
+            try {
+                val deferredFavorites = async(Dispatchers.IO) {
+                    // CONTENT_URI = content://com.dicoding.auliarosyida.githubuser/user
+                    val cursor = contentResolver.query(CONTENT_URI, null, null, null, null)
+                    MappingHelper.mapCursorToArrayList(cursor)
                 }
-                Toast.makeText(this@MainActivity, errorMessage, Toast.LENGTH_SHORT).show()
-            }
-        })
-    }
 
-    private fun getListUsers(response: String) {
-        val listUser = ArrayList<User>()
-
-        try{
-            val responseObject = JSONObject(response)
-            val dataArray = responseObject.getJSONArray("items")
-
-            val gson = Gson()
-            for(i in 0 until dataArray.length()){
-                val dataObject = dataArray.getJSONObject(i)
-                val data = gson.fromJson(dataObject.toString(), User::class.java)
-                data.id = i
-                listUser.add(data)
+                val favorites = deferredFavorites.await()
+                if (favorites.size > 0) {
+                    adapterFavPage.listFavorites = favorites
+                } else {
+                    var listTemp = ArrayList<User>()
+                    listTemp.add(dummyFavorite)
+                    adapterFavPage.listFavorites = listTemp
+                    showSnackbarMessage("Tidak ada data saat ini")
+                }
+                binding.progressBar.visibility = View.INVISIBLE
+            }catch (e : Exception){
+                e.printStackTrace()
             }
-
-            if(listUser.size == 0){
-                users.clear()
-                users.add(dummyUser)
-            }
-            else if(users.size == 0 )users.addAll(listUser)
-            else{
-                users.clear()
-                users.addAll(listUser)
-            }
-            showRecyclerList(users)
-        } catch (e: Exception) {
-            Toast.makeText(this@MainActivity, e.message, Toast.LENGTH_SHORT).show()
-            e.printStackTrace()
         }
     }
-    private fun showRecyclerList(usersTemp: MutableList<User>) {
-        listUserAdapter = UserAdapter(usersTemp)
-        binding.rvList.adapter?.notifyDataSetChanged();
+
+    private fun showSnackbarMessage(message: String) {
+        Snackbar.make(binding.rvList, message, Snackbar.LENGTH_SHORT).show()
     }
 
-
-    private fun getDetailUserApi(aUser: User, position: Int) {
-        val clientDetail = AsyncHttpClient()
-       clientDetail.addHeader("Authorization", "token ${BuildConfig.GITHUB_TOKEN}")
-       clientDetail.addHeader("User-Agent", "request")
-        val url = "https://api.github.com/users/${aUser.username}"
-
-        clientDetail.get(url, object : AsyncHttpResponseHandler() {
-            override fun onSuccess(statusCode: Int, headers: Array<Header>, responseBody: ByteArray) {
-                // Parsing JSON
-                val resultDetail = String(responseBody)
-
-                val gson = Gson()
-                val dataObject = JSONObject(resultDetail)
-                val newUser = gson.fromJson(dataObject.toString(), User::class.java)
-                newUser.id = position
-                showSelectedUser(newUser)
-            }
-            override fun onFailure(statusCode: Int, headers: Array<Header>, responseBody: ByteArray, error: Throwable) {
-                // Jika koneksi gagal
-                binding.progressBar.visibility = View.INVISIBLE
-                val errorMessage = when (statusCode) {
-                    401 -> "$statusCode : Bad Request"
-                    403 -> "$statusCode : Forbidden"
-                    404 -> "$statusCode : Not Found"
-                    else -> "$statusCode : ${error.message}"
-                }
-                Toast.makeText(this@MainActivity, errorMessage, Toast.LENGTH_SHORT).show()
-            }
-        })
-    }
-
-    private fun showSelectedUser(user: User) {
-        val moveWithObjectIntent = Intent(this@MainActivity, TabLayoutActivity::class.java)
-        moveWithObjectIntent.putExtra(TabLayoutActivity.EXTRA_USER, user)
-        startActivity(moveWithObjectIntent)
+    override fun onSaveInstanceState(outState: Bundle) {
+        super.onSaveInstanceState(outState)
+        outState.putParcelableArrayList(EXTRA_STATE, adapterFavPage.listFavorites)
     }
 }
